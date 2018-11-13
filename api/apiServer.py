2ceb786466ad23ec483ed93aeb4601cffb71588d
zhangyoulinag <1577121881@qq.com>
2018-11-13 17:58:37 +0800

    优化同一个wifi下面存在多个用户问题

--- api/apiServer.py
+++ api/apiServer.py
@@ -4,40 +4,31 @@
 '''
 import json
 import sys
-import web
 import config
 from db.DataStore import sqlhelper
 from db.SqlHelper import Proxy
+from flask import Flask
+from flask import request
 
-urls = (
-    '/', 'select',
-    '/delete', 'delete'
-)
+app = Flask(__name__)
+app.config.update(DEBUG=False)
+
+@app.route("/",methods=['GET'])
+def home():
+    count = request.args.get('count',None)
+    datax = request.args.to_dict()
+    json_result = json.dumps(sqlhelper.select(count, datax))
+    return json_result
+
+@app.route("/delete",methods=['GET'])
+def delete():
+    inputs = web.input()
+    json_result = json.dumps(sqlhelper.delete(inputs))
+    return json_result
 
 
 def start_api_server():
-    sys.argv.append('0.0.0.0:%s' % config.API_PORT)
-    app = web.application(urls, globals())
     app.run()
-
-
-class select(object):
-    def GET(self):
-        inputs = web.input()
-        json_result = json.dumps(sqlhelper.select(inputs.get('count', None), inputs))
-        return json_result
-
-
-class delete(object):
-    params = {}
-
-    def GET(self):
-        inputs = web.input()
-        json_result = json.dumps(sqlhelper.delete(inputs))
-        return json_result
-
 
 if __name__ == '__main__':
-    sys.argv.append('0.0.0.0:8000')
-    app = web.application(urls, globals())
-    app.run()
+    start_api_server()
