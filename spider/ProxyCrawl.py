fa1492c6d60d8b58771409db4d3a5e3f355cb66c
zhangyoulinag <1577121881@qq.com>
2019-03-30 23:20:05 +0800

    remove old api

--- spider/ProxyCrawl.py
+++ spider/ProxyCrawl.py
@@ -9,7 +9,6 @@ import gevent
 from gevent.pool import Pool
 from multiprocessing import Queue, Process, Value
 
-from api.apiServer import start_api_server
 from config import THREADNUM, parserList, UPDATE_TIME, MINNUM, MAX_CHECK_CONCURRENT_PER_PROCESS, MAX_DOWNLOAD_CONCURRENT
 from db.DataStore import store_data, sqlhelper
 from spider.HtmlDownloader import Html_Downloader
@@ -98,12 +97,10 @@ if __name__ == "__main__":
     DB_PROXY_NUM = Value('i', 0)
     q1 = Queue()
     q2 = Queue()
-    p0 = Process(target=start_api_server)
     p1 = Process(target=startProxyCrawl, args=(q1, DB_PROXY_NUM, myip))
     p2 = Process(target=validator, args=(q1, q2, myip))
     p3 = Process(target=store_data, args=(q2, DB_PROXY_NUM))
 
-    p0.start()
     p1.start()
     p2.start()
     p3.start()
